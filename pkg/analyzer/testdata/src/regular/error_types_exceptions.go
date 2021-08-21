b985402400511a7e16a0e57132b48fb92f063729
Антон Телышев <anton.telishev@yandex.ru>
2021-08-21 12:58:43 +0300

    Improve msg for array types

--- pkg/analyzer/testdata/src/regular/error_types_exceptions.go
+++ pkg/analyzer/testdata/src/regular/error_types_exceptions.go
@@ -4,12 +4,28 @@ import "strings"
 
 type ValidationErrors []string
 
-func (ve ValidationErrors) Error() string {
-	return strings.Join(ve, "\n")
-}
+func (ve ValidationErrors) Error() string { return strings.Join(ve, "\n") }
+
+type validationErrors []string
+
+func (ve validationErrors) Error() string { return strings.Join(ve, "\n") }
 
 type TenErrors [10]string
 
-func (te TenErrors) Error() string {
-	return strings.Join(te[:], "\n")
-}
+func (te TenErrors) Error() string { return strings.Join(te[:], "\n") }
+
+type tenErrors [10]string
+
+func (te tenErrors) Error() string { return strings.Join(te[:], "\n") }
+
+type MultiError []error             // want "the type name `MultiError` should conform to the `XxxErrors` format"
+func (me MultiError) Error() string { return "" }
+
+type multiError []error             // want "the type name `multiError` should conform to the `xxxErrors` format"
+func (me multiError) Error() string { return "" }
+
+type TwoError [2]error            // want "the type name `TwoError` should conform to the `XxxErrors` format"
+func (te TwoError) Error() string { return te[0].Error() + "\n" + te[1].Error() }
+
+type twoError [2]error            // want "the type name `twoError` should conform to the `xxxErrors` format"
+func (te twoError) Error() string { return te[0].Error() + "\n" + te[1].Error() }
